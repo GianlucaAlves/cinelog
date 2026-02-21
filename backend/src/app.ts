7eb84318b188ceb733149f83cdc420c89a1f5bc9
Duraxxi <alves.gian@ymail.com>
2026-02-21 09:31:56 -0300

    Built the frontend to consume the backend API, added CORS middleware to the backend, and created types for TMDB data. Also added some UI components for the frontend.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,14 +2,20 @@ import express from 'express';
 import router from './routes/auth';
 import cookieParser from 'cookie-parser';
 import tmdbRouter from './routes/tmdb';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true 
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
 app.use('/', tmdbRouter);
+
 
 app.get('/', (req, res) => {
   res.json({ok: true});
--- frontend/cinelog/src/App.tsx
+++ frontend/cinelog/src/App.tsx
@@ -1,13 +1,40 @@
-
+// src/App.tsx
+import { useEffect, useState } from 'react'
+import { MovieCard } from '@/components/MovieCard'
+import type { Movie, TMDBResponse } from '../../../backend/src/types/tmdbTypes'
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+  useEffect(() => {
+  const fetchMovies = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(`${API_BASE_URL}/movies/popular`);
+      const data: TMDBResponse = await res.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchMovies();
+}, []);
+
+  
+
+  if (loading) return <p className="text-center mt-10">Loading...</p>;
 
   return (
-    <>
-      <h1>Cinelog</h1>
-    </>
-  )
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6">
+      {movies.map(movie => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
